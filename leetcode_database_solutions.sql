c0481b3fac7374e2079be724f37b7144f93cd17c
Vansh Jain <146535124+VanshJain36@users.noreply.github.com>
2026-03-20 22:53:34 +0530

    Add SQL queries for duplicate emails and customers

--- leetcode_database_solutions.sql
+++ leetcode_database_solutions.sql
@@ -12,3 +12,22 @@ select name as Employee
 from employee as e 
 where 
 salary > (select salary from employee where id = e.managerId);
+
+-- 182. Duplicate Emails
+
+select 
+email as Email 
+from person 
+group by email 
+having 
+count(email) > 1;
+
+-- 183. Customers who never order
+
+select c.name as Customers 
+from customers c 
+left join 
+orders o on 
+(c.id = o.customerId) 
+where 
+o.id is Null;
