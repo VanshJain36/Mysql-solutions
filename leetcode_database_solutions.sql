067ef0e53cae0b91e4011f243273a59c5d35bc73
Vansh Jain <146535124+VanshJain36@users.noreply.github.com>
2026-03-27 19:24:09 +0530

    Add SQL query for reformatting department table

--- leetcode_database_solutions.sql
+++ leetcode_database_solutions.sql
@@ -140,3 +140,22 @@ from activity
 where activity_date > '2019-06-27' 
 and activity_date <= '2019-07-27' 
 group by activity_date;
+
+-- 1179. Reformat Department Table
+
+select 
+    id,
+    sum(case when month = 'Jan' then revenue end) as Jan_Revenue,
+    sum(case when month = 'Feb' then revenue end) as Feb_Revenue,
+    sum(case when month = 'Mar' then revenue end) as Mar_Revenue,
+    sum(case when month = 'Apr' then revenue end) as Apr_Revenue,
+    sum(case when month = 'May' then revenue end) as May_Revenue,
+    sum(case when month = 'Jun' then revenue end) as Jun_Revenue,
+    sum(case when month = 'Jul' then revenue end) as Jul_Revenue,
+    sum(case when month = 'Aug' then revenue end) as Aug_Revenue,
+    sum(case when month = 'Sep' then revenue end) as Sep_Revenue,
+    sum(case when month = 'Oct' then revenue end) as Oct_Revenue,
+    sum(case when month = 'Nov' then revenue end) as Nov_Revenue,
+    sum(case when month = 'Dec' then revenue end) as Dec_Revenue
+from department 
+group by id;
