f15c37b98ecbfa09bdfc6f702d04d387ff501b26
Vansh Jain <146535124+VanshJain36@users.noreply.github.com>
2026-04-05 22:44:28 +0530

    Add query for latest login in 2020
    Add SQL query to find the latest login for each user in 2020.

--- leetcode_database_solutions.sql
+++ leetcode_database_solutions.sql
@@ -315,3 +315,11 @@ else salary
 end as bonus
 from employees
 order by employee_id;
+
+-- 1890. The Latest Login in 2020
+
+select distinct user_id , max(time_stamp) as last_stamp 
+from logins 
+where time_stamp 
+between '2020-01-01 00:00:00' and '2020-12-31 23:59:59' 
+group by user_id;
