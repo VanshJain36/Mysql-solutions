adf9ea6dcdec493b264a87728f08a681399f42ec
Vansh Jain <146535124+VanshJain36@users.noreply.github.com>
2026-04-10 19:31:40 +0530

    Add SQL queries for ranking scores and finding consecutive numbers

--- leetcode_database_solutions(medium).sql
+++ leetcode_database_solutions(medium).sql
@@ -17,3 +17,17 @@ BEGIN
       limit 1
   );
 END
+
+  -- 178. Rank Scores
+
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) as 'rank'
+FROM Scores;
+
+-- 180. Consecutive Numbers
+
+select distinct l1.num as ConsecutiveNums 
+from logs l1, logs l2, logs l3
+where l1.num = l2.num
+and l2.num = l3.num
+and l1.id = l2.id + 1
+and l2.id = l3.id + 1;
