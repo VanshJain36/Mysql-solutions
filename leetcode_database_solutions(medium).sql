feae0e5be07104f08c031bc493c31480724d3d55
Vansh Jain <146535124+VanshJain36@users.noreply.github.com>
2026-04-11 16:26:13 +0530

    Add query for department highest salary
    Add SQL query for finding highest salary in each department.

--- leetcode_database_solutions(medium).sql
+++ leetcode_database_solutions(medium).sql
@@ -31,3 +31,15 @@ where l1.num = l2.num
 and l2.num = l3.num
 and l1.id = l2.id + 1
 and l2.id = l3.id + 1;
+
+-- 184. Department Highest Salary
+
+select d.name as department, e.name as employee, e.salary as salary 
+from employee e 
+join department d
+on (e.departmentid = d.id)
+where (e.departmentid, e.salary) in (
+    select departmentid, max(salary) 
+    from employee 
+    group by departmentid
+);
