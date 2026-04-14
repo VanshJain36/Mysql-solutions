68fdb72d4facbf355e8234ae8a54c3392bad0098
Vansh Jain <146535124+VanshJain36@users.noreply.github.com>
2026-04-14 19:18:32 +0530

    Refactor SQL query for customer product count

--- leetcode_database_solutions(medium).sql
+++ leetcode_database_solutions(medium).sql
@@ -115,7 +115,7 @@ order by id asc;
 
 -- 1045. Customers Who Bought All Products
 
-select customer_id 
-from customer 
-group by customer_id
-having count(distinct product_key) = (select count(product_key) from product);
+select c.customer_id 
+from customer c
+group by c.customer_id
+having count(distinct c.product_key) = (select count(p.product_key) from product p);
